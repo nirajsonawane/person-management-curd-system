2a4c971ff6e0dfec5c2e1ac111cc93589454c34e
nirajsonawane <niraj.sonawane@gmail.com>
2020-01-19 19:33:06 +0200

    Added Project for Reactive API

---
+++ person-management-api-reactive/src/test/java/com/api/PersonManagementApiReactiveApplicationTests.java
@@ -0,0 +1,34 @@
+package com.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.util.TestPropertyValues;
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+
+@SpringBootTest
+@Slf4j
+@ContextConfiguration(initializers = PersonManagementApiReactiveApplicationTests.MongoDbInitializer.class)
+
+class PersonManagementApiReactiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+	public static class MongoDbInitializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+		@Override
+		public void initialize(ConfigurableApplicationContext configurableApplicationContext) {
+			log.info("Overriding Spring Properties for mongodb !!!!!!!!!");
+
+			TestPropertyValues values = TestPropertyValues.of(
+					"spring.data.mongodb.host=" + "localhost"
+
+			);
+			values.applyTo(configurableApplicationContext);
+		}
+	}
+}
