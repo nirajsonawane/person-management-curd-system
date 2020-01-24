f90ac20195e4fd60425da0168b8e01fad331d510
nirajsonawane <niraj.sonawane@gmail.com>
2020-01-24 02:49:12 +0200

    Small Corrections

--- person-management-api-reactive/src/main/java/com/api/PersonManagementApiReactiveApplication.java
+++ person-management-api-reactive/src/main/java/com/api/PersonManagementApiReactiveApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.reactive.config.EnableWebFlux;
@@ -24,29 +25,6 @@ public class PersonManagementApiReactiveApplication {
 		SpringApplication.run(PersonManagementApiReactiveApplication.class, args);
 	}
 
-	@Bean
-	ApplicationRunner init(PersonRepository  repository) {
-		return (ApplicationArguments args) ->  dataSetup(repository);
-	}
 
-	private void dataSetup(PersonRepository repository) {
-
-		 IntStream
-				.range(1, 10)
-				.mapToObj(it -> getData(it))
-				.map(it->repository.save(it).block())
-				.count();
-	}
-
-	Person getData(int number){
-		return Person
-				.builder()
-				.age(number)
-				.favouriteColour(Colour.BLACK)
-				.firstName("User " + number)
-				.lastName("Test")
-				.hobby(Arrays.asList(Hobby.builder().hobbyName("Test").build()))
-				.build();
-	}
 
 }
